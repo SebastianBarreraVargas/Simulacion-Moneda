c81dc2faba4c3c4ef25c4ea222bc62c2d629ceb2
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-09-07 20:02:49 -0400

    feat: Agrega la logica de la funcion

---
+++ model/Lanzamiento-Moneda.py
@@ -0,0 +1,18 @@
+import random
+
+def simular_moneda(lado):
+    lanzamientos = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+    probabilidades = []
+
+    for n in lanzamientos:
+        favorables = 0
+
+        for _ in range(n):
+            resultado = random.randint(0, 1)
+            if resultado == lado:
+                favorables += 1
+
+        probabilidad = (favorables / n) * 100
+        probabilidades.append(probabilidad)
+
+    return tuple(probabilidades)
