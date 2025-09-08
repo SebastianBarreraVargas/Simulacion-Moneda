6a331a5ad25ff0380b57593d13ed098378ee0ab7
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2025-09-07 20:34:54 -0400

    feat: Agrega el main

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py  (poner en la raíz del proyecto)
+import sys
+from pathlib import Path
+import tkinter as tk
+from model.lanzamiento_moneda import simular_moneda
+from view.vista import App
+
+def main():
+    root = tk.Tk()
+    app = App(root, simular_moneda)  
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
